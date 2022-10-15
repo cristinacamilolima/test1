bcae1d21bd8c4673e5df2c6e9ec06d436907a896
Cristina Camilo <7895168+paulolimarb@users.noreply.github.com>
2022-10-15 16:39:56 -0300

    second commit

--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package test1
 import "fmt"
 
 func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf("Hi, %v. Welcome! new version", name)
 	return message
 }
