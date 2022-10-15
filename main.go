50e6f50a8e1dded12178a7796157fc568c60f336
Cris Camilo <cristinacamilo.lima@gmail.com>
2022-10-15 16:56:12 -0300

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package test1
 import "fmt"
 
 func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v. Welcome! new version", name)
+	message := fmt.Sprintf("Hi, %v. Welcome! new version - xpto", name)
 	return message
 }
